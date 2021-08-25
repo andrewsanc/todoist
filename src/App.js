e7eddcf2e8164474a47c2ad96d43776e4987eb70
TB831 <druu831@gmail.com>
2021-08-25 14:15:34 -0700

    added tailwind dependency. tailwind boilerplate component for testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div class='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
+      <div class='flex-shrink-0'>
+        <img class='h-12 w-12' src='/logo192.png' alt='ChitChat Logo' />
+      </div>
+      <div>
+        <div class='text-xl font-medium text-black'>ChitChat</div>
+        <p class='text-gray-500'>You have a new message!</p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
