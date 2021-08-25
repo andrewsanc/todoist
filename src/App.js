3671df5eff46c1831b2f6ffa69cfc870581af1bf
TB831 <druu831@gmail.com>
2021-08-25 14:24:57 -0700

    card boilerplate component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
+import Card from "./Card";
 
 const App = () => {
-  return (
-    <div class='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
-      <div class='flex-shrink-0'>
-        <img class='h-12 w-12' src='/logo192.png' alt='ChitChat Logo' />
-      </div>
-      <div>
-        <div class='text-xl font-medium text-black'>ChitChat</div>
-        <p class='text-gray-500'>You have a new message!</p>
-      </div>
-    </div>
-  );
+  return <Card />;
 };
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div class='bg-white p-6 rounded-lg shadow-lg'>
+      <h2 class='text-2xl font-bold mb-2 text-gray-800'>Card with no image</h2>
+      <p class='text-gray-700'>This is my cool new card!</p>
+    </div>
+  );
+};
+
+export default Card;
