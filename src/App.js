ce7844d3933e9965e1c7bbf2f69fdb036eaf9180
TB831 <druu831@gmail.com>
2021-11-16 15:28:47 -0800

    add todo item funcitonality to local state variable

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 import Card from "./Card";
 import Form from "./Form";
 
-const initialState = [
-  { title: "My first todo", description: "Weeee!" },
-  { title: "My second todo", description: "WEEEEEE!" },
-];
+const initialState = ["Pick up groceries", "Order new mouse", "Feed Luci"];
 
 const App = () => {
   const [toDoList, setToDoList] = useState(initialState);
 
-  const renderToDoList = toDoList.map((toDoObj) => {
-    return <Card title={toDoObj.title} description={toDoObj.description} />;
+  const renderToDoList = toDoList.map((item, i) => {
+    return <Card key={`ToDo Item-${i}`} title={item} />;
   });
 
-  const showCreateNewToDo = () => {};
+  const submitToDoHandler = (e, newToDoItem, setNewToDoItem) => {
+    e.preventDefault();
+    if (newToDoItem === "") return;
+    let newToDoList = [...toDoList];
+    newToDoList.push(newToDoItem);
+    setToDoList(newToDoList);
+    setNewToDoItem("");
+  };
 
   return (
     <div className='bg-gray-100 flex flex-col items-center h-screen'>
       <div className='container w-7/12'>
-        <Form />
+        <Form submitToDoHandler={submitToDoHandler} />
         <div>{renderToDoList}</div>
       </div>
     </div>
--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ submitToDoHandler }) => {
+  const [newToDoItem, setNewToDoItem] = useState("");
+
   return (
     <div className='bg-white my-10 p-2 border rounded-md shadow-s flex flex-col items-center'>
       <h1 className='text-2xl font-medium my-3'>Task Manager</h1>
       <div className='container mb-8 flex items-center justify-center'>
-        <input
-          class='shadow border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-8/12 rounded-l-md'
-          type='text'
-          placeholder='Title'
-        />
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-md'>
-          Submit
-        </button>
+        <form
+          onSubmit={(e) => submitToDoHandler(e, newToDoItem, setNewToDoItem)}
+        >
+          <input
+            onChange={(e) => setNewToDoItem(e.target.value)}
+            value={newToDoItem}
+            className='shadow border py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-8/12 rounded-l-md'
+            type='text'
+            placeholder='e.g. pick up groceries'
+          />
+          <button
+            type='submit'
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-md'
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
